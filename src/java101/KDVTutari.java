e8764b37955e41826885926740ed77b63c442d2e
Ayhan <atezyapar@gmail.com>
2022-04-04 00:13:31 +0300

    KDVTutari eklendi.

---
+++ src/java101/KDVTutari.java
@@ -0,0 +1,27 @@
+package java101;
+import java.util.Scanner;
+
+/*
+Fiyatý girilen ürünün KDV'li fiyatýný hesaplayan program.
+ */
+
+public class KDVTutari {
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("KDV'siz fiyatý giriniz: ");
+		int fiyat = input.nextInt();
+		
+		int kdv = (fiyat < 1000) ? 18 : 8;
+		double kdv_tutar = fiyat * kdv / 100;
+		double kdv_fiyat = fiyat + kdv_tutar;
+		
+		System.out.print("KDV tutarý: ");
+		System.out.println(kdv_tutar);
+		System.out.print("KDV'li fiyat: ");
+		System.out.println(kdv_fiyat);
+	}
+
+}
