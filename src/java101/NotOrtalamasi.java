805206d30e50fa4c9626286eca05d3b006140dc0
Ayhan <atezyapar@gmail.com>
2022-04-03 23:10:17 +0300

    NotOrtalamasi eklendi.

---
+++ src/java101/NotOrtalamasi.java
@@ -0,0 +1,43 @@
+package java101;
+import java.util.Scanner;
+
+/*
+6 dersin not ortalamasýný hesaplayan ve buna göre 
+sýnýfýn geçilip geçilemediðini belirleyen Java programý
+*/
+public class NotOrtalamasi {
+
+	public static void main(String[] args) {
+		
+		int mat, fiz, kim, tur, tar, muz;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Matematik Notunuzu Giriniz: ");
+		mat = input.nextInt();
+		
+		System.out.print("Fizik Notunuzu Giriniz: ");
+		fiz = input.nextInt();
+		
+		System.out.print("Kimya Notunuzu Giriniz: ");
+		kim = input.nextInt();
+		
+		System.out.print("Türkçe Notunuzu Giriniz: ");
+		tur = input.nextInt();
+		
+		System.out.print("Tarih Notunuzu Giriniz: ");
+		tar = input.nextInt();
+		
+		System.out.print("Müzik Notunuzu Giriniz: ");
+		muz = input.nextInt();
+		
+		int toplam = mat + fiz + kim + tur + tar + muz;
+		double ortalama = toplam / 6;
+		
+		String durum = (ortalama >= 60) ? "Sýnýfý Geçtiniz." : "Sýnýfta Kaldýnýz.";
+		System.out.print("Ortalamaniz: ");
+		System.out.println(ortalama);
+		System.out.println(durum);
+	}
+
+}
